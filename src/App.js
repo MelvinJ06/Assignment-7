c97ce7951c5ff3ae6f5e081367ca087e9fe343e9
melvinjames <melvinjames0602@gmail.com>
2024-09-25 10:59:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+import HomePage from "./Pages/HomePage";
+
+import MoviePage from "./Pages/MoviePage";
+
+import './Styles/styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" Component = {HomePage} />
+          <Route path="/movies/:id" Component = {MoviePage} />
+        </Routes>
+      </Router>
   );
 }
 
